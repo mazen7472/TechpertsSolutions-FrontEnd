48ed6cd9f7573e7a33254bf8119cdeb09ba08871
marawangomaa <116531019+marawangomaa@users.noreply.github.com>
2025-07-18 19:37:20 +0300

    products & prands in home

--- src/app/components/home/components/home-prands/home-prands.component.ts
+++ src/app/components/home/components/home-prands/home-prands.component.ts
@@ -8,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './home-prands.component.css'
 })
 export class HomePrandsComponent {
-
+  paused = false;
+  brands = [
+    { name: 'ASUS', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/6/6e/ASUS_Logo.svg' },
+    { name: 'MSI', imageUrl: 'https://upload.wikimedia.org/wikipedia/en/2/29/Msi_Logo.svg' },
+    { name: 'Gigabyte', imageUrl: 'https://upload.wikimedia.org/wikipedia/en/0/0b/Gigabyte_Technology_logo.svg' },
+    { name: 'Corsair', imageUrl: 'https://upload.wikimedia.org/wikipedia/en/3/35/Corsair_Logo.svg' },
+    { name: 'NVIDIA', imageUrl: 'https://upload.wikimedia.org/wikipedia/en/2/21/Nvidia_logo.svg' },
+    { name: 'AMD', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/2/2e/AMD_Logo.svg' },
+    { name: 'Intel', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/c/c9/Intel-logo.svg' },
+    { name: 'Cooler Master', imageUrl: 'https://upload.wikimedia.org/wikipedia/en/e/ef/Cooler_Master_logo.svg' },
+    { name: 'NZXT', imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/5/5d/NZXT_logo.svg' },
+    { name: 'EVGA', imageUrl: 'https://upload.wikimedia.org/wikipedia/en/0/0a/EVGA_logo.svg' }
+  ];
 }
--- src/app/components/home/components/home-products/home-products.component.ts
+++ src/app/components/home/components/home-products/home-products.component.ts
@@ -1,12 +1,55 @@
 import { Component } from '@angular/core';
+import { IProduct } from '../../../../Interfaces/iproduct';
+import { ProductService } from '../../../../Services/product.service';
+import { CommonModule } from '@angular/common';
+import { CartService } from '../../../../Services/cart.service';
 
 @Component({
   selector: 'app-home-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home-products.component.html',
   styleUrl: './home-products.component.css'
 })
 export class HomeProductsComponent {
+  products: IProduct[] = [];
+  loading = false;
+  error = '';
 
+  constructor(
+    private productService: ProductService,
+    private cartService: CartService
+  ) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(): void {
+    this.loading = true;
+    this.productService.getAllProducts(1, 10, 'name', false).subscribe({
+      next: (res) => {
+        this.products = res.data.items;
+        this.loading = false;
+      },
+      error: () => {
+        this.error = 'Failed to load products.';
+        this.loading = false;
+      }
+    });
+  }
+
+  addToCart(product: IProduct): void {
+    const cartItem = { productId: product.id, quantity: 1 };
+    this.cartService.addItem(cartItem).subscribe({
+      next: () => {
+        this.cartService.updateCartState([cartItem]);
+        alert(`${product.name} added to cart`);
+      },
+      error: (err) => {
+        console.error(err);
+        alert('Could not add product to cart');
+      }
+    });
+  }
 }
